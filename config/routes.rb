d2fb1d283b61a7dac88b1c4edf6490aa03107e98
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2019-10-01 19:11:28 -0500

    Add a failing route for when authorization goes wrong. And add a new simple `login` route to redirect users to Omniauth default endpoint.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+	get '/login', to: redirect('/auth/github')
 	get '/auth/:provider/callback', to: 'sessions#create'
+	get 'auth/failure', to: redirect('/')
+
+	resource :session, only: [:create]
 
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
